1490d9ab663bfb742ee0e574b08d323eb0e3c3a8
nataraz <natarazjavaarena@gmail.com>
2024-07-12 11:58:26 +0530

    adding  more tests

---
+++ GITMavenProj01/src/test/java/AppTest.java
@@ -0,0 +1,38 @@
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.nt.service.Arithmetic;
+
+public class AppTest {
+	
+	
+	@Test
+	public void  testWithPositives() {
+		Arithmetic ar=new Arithmetic();
+		int expected =40;
+		int actual=ar.sum(20, 20);
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void  testWithNegetives() {
+		Arithmetic ar=new Arithmetic();
+		int expected =-40;
+		int actual=ar.sum(-20, -20);
+		assertEquals(expected, actual);
+		
+	}
+
+	@Test
+	public void  testWithZeros() {
+		Arithmetic ar=new Arithmetic();
+		int expected =0;
+		int actual=ar.sum(0, 0);
+		assertEquals(expected, actual);
+		
+	}
+}
