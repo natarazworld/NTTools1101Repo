ddd37a5756f75064a683d48ec74d227aa797acc9
nataraz <natarazjavaarena@gmail.com>
2024-07-12 11:46:22 +0530

    Dev1- Arithemetic operation

--- GITMavenProj01/src/main/java/com/nt/main/ApplicationRun.java
+++ GITMavenProj01/src/main/java/com/nt/main/ApplicationRun.java
@@ -1,9 +1,12 @@
 package com.nt.main;
 
+import com.nt.service.Arithmetic;
+
 public class ApplicationRun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Arithmetic  ar=new Arithmetic();
+		System.out.println("sum is ::"+ar.sum(10, 20));
 
 	}
 
