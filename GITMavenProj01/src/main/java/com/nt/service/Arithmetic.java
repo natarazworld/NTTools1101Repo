fc78cf345062bbb1233fc94cef30180516fe8479
nataraz <natarazjavaarena@gmail.com>
2024-07-12 12:17:21 +0530

    sum(-,-) from dev1 machine

--- GITMavenProj01/src/main/java/com/nt/service/Arithmetic.java
+++ GITMavenProj01/src/main/java/com/nt/service/Arithmetic.java
@@ -3,7 +3,7 @@ package com.nt.service;
 public class Arithmetic {
 	
 	public int sum(int x, int y) {
-		System.out.println("Arithmetic.sum() (begin)");
+		System.out.println("Arithmetic.sum() (begin)"); 
 		return x+y;
 	}
 
